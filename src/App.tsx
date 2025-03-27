2ca8e4f1555dd67271ef60bc42234a27e87126a6
Rajesh97266 <rajesh97266@gmail.com>
2025-03-27 14:00:26 +0530

    initialize project structure with Vite, React, and TypeScript; add Tailwind CSS and utility components

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import { BrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+
+function App() {
+  return (
+   <BrowserRouter>
+   <Layout>
+Hello
+   </Layout>
+   </BrowserRouter>
+  );
+}
+
+export default App;
