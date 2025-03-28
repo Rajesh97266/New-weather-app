c92e63fdfc1b4d5fd3f36b3f0c2cb211678bae68
Rajesh97266 <rajesh97266@gmail.com>
2025-03-28 17:27:47 +0530

    weather-dashboard-page-updated

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,9 +1,70 @@
-import React from "react";
+import WeatherSkeleton from "@/components/loading-skeleton";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import { useGeolocation } from "@/hooks/use-geolocation";
+import { MapPin, RefreshCw } from "lucide-react";
+import { AlertCircle } from "lucide-react";
 
 const WeatherDashboard = () => {
+  const {
+    coordinates,
+    isLoading: locationLoading,
+    getLocation,
+    error: locationError,
+  } = useGeolocation();
+
+  const handleRefresh = () => {
+    getLocation();
+    // if (coordinates) {
+
+    // }
+  };
+
+  if (locationLoading) {
+    return <WeatherSkeleton />;
+  }
+
+  if (locationError) {
+    return (
+      <Alert variant="destructive">
+        <AlertCircle className="h-4 w-4" />
+        <AlertTitle>Location Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>{locationError}</p>
+          <Button onClick={getLocation} variant={"outline"} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  if (!coordinates) {
+    return (
+      <Alert>
+        <MapPin className="h-4 w-4" />
+        <AlertTitle>Location Required</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>Please enable location access to see your local weather.</p>
+          <Button variant="outline" onClick={getLocation} className="w-fit">
+            <MapPin className="mr-2 h-4 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
   return (
-    <div>
-      <h1>Weather Dashboard</h1>
+    <div className="space-y-4">
+      {/* Favorite Cities */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+        <Button variant="outline" className="w-fit" onClick={handleRefresh}>
+          <RefreshCw className=" h-4 w-4" />
+        </Button>
+      </div>
     </div>
   );
 };
